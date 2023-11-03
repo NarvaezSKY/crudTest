21a459ea1b80d019f6d8b8a42d1f7ee52e0f8cf8
cristian <crissnar12xd@gmail.com>
2023-11-03 18:31:13 -0500

    backend funcional

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import {Schema, model} from "mongoose";
+
+const userSchema=new Schema(
+    {
+        firstname:{type:String},
+        lastname:{type:String},
+        image:{type:String},
+    
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const User=model('User', userSchema)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,8 @@
 import express from "express"
-import {upload} from "../util/upload.js";
+import {upload} from "../config/multer.js";
+import {uploadFile} from '../util/uploadFile.js'
+import {User} from '../models/User.js'
+
 
 const router=express()
 
@@ -7,8 +10,24 @@ router.get('/user',(req,res)=>{
     return res.json({message:'get user'})
 })
 
-router.post('/createuser',(req,res)=>{
-    return res.json({message:'create user'})
+router.post('/createuser',upload.fields([{name: 'image', maxCount:1}]), async(req,res)=>{
+    const body=req.body
+    const image=req.files.image
+
+    if (image&&image.length>0){
+        const {dowloadURL}=await uploadFile(image[0])
+
+        const newUser=await new User({
+            firstname: body.firstname,
+            lastname: body.lastname,
+            image: dowloadURL
+        }).save()
+        return res.status(200).json({
+            newUser
+        })
+    }
+
+    return res.status(400).json({message:'missing image in req.body'})
 })
 
 export default router
